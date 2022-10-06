800c36ca0865e130d61a58e2c5b04340ba45d3fc
Aleksandra Laikhtman <alaikhtman@gmail.com>
2022-10-06 17:02:45 +0300

    add helloApi test and helloName

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
+
     @Test
-    public void TestHelloWorld() {
-        System.out.println("Hello World");
+    public void testHelloWorld() {
+      System.out.println("Hello from Sasha");
+    }
+
+    @Test
+    public void testHelloAPI() {
+     Response response = RestAssured
+             .get("https://playground.learnqa.ru/api/hello")
+             .andReturn();
+     response.prettyPrint();
     }
 }
