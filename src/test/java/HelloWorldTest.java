18848bc09ab0e361c762ad098ae935438ecdfa74
Aleksandra Laikhtman <alaikhtman@gmail.com>
2022-10-16 00:32:35 +0300

    add get JsonList test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,22 +1,25 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloWorldTest {
 
 
     @Test
     public void testHelloWorld() {
-      System.out.println("Hello from Sasha");
+        System.out.println("Hello from Sasha");
     }
 
     @Test
     public void testHelloAPI() {
-     Response response = RestAssured
-             .get("https://playground.learnqa.ru/api/hello")
-             .andReturn();
-     response.prettyPrint();
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 
     @Test
@@ -26,4 +29,17 @@ public class HelloWorldTest {
                 .andReturn();
         response.prettyPrint();
     }
+
+    @Test
+    public void testGetJSON() {
+
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+      List<String> messages = response.getList("messages.message");
+      System.out.println(messages.get(1));
+    }
+
+
 }
